e890f9e052aa828652542da18cb14e3a287f08d0
Ramzi-B <Ramzi.B@gmx.com>
2025-02-15 09:30:41 +0100

    feat: Create App\Enum\GenderEnum, add new field gender in Product entity

---
+++ src/Enum/GenderEnum.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Enum;
+
+/**
+ * Enum representing the gender of a product.
+ * 
+ */
+enum GenderEnum: string
+{
+    case MEN = 'homme';
+    case WOMEN = 'femme';
+    case UNISEX = 'unisexe';
+    case KIDS = 'enfant';
+    case BABY = 'bebe';
+    case BOY = 'garcon';
+    case GIRL = 'fille';
+
+    /**
+     * Get the label associated with the gender.
+     * 
+     * @return string The label associated with the gender.
+     */
+    public function getLabel(): string
+    {
+        return match($this) {
+            self::MEN => 'Homme',
+            self::WOMEN => 'Femme',
+            self::UNISEX => 'Unisexe',
+            self::KIDS => 'Enfant',
+            self::BABY => 'Bebe',
+            self::BOY => 'Garcon',
+            self::GIRL => 'Fille',
+        };
+    }
+
+    /**
+     * Get the array of choices.
+     * 
+     * @return array<string, string>
+     */
+    public static function getChoices(): array
+    {
+        return array_combine(
+            array_map(fn($case) => $case->value, self::cases()),
+            array_map(fn($case) => $case->getLabel(), self::cases())
+        );
+    }
+}
